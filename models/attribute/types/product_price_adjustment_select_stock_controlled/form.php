b7e5c00b2852f266ad5018407d0151d041f3f6c0
Ryan Hewitt <ryan@mesuva.com.au>
2014-10-08 12:34:02 +1030

    Adds the attribute's name to options
    Adds the attribute's name to the blank first field in the attribute
    drop down’s, as it’s more descriptive than just an empty option

--- models/attribute/types/product_price_adjustment_select_stock_controlled/form.php
+++ models/attribute/types/product_price_adjustment_select_stock_controlled/form.php
@@ -3,25 +3,24 @@
 $options = $this->controller->getOptions();
 $form = Loader::helper('form');
 
-$opts = array('');
+$opts = array('- ' . $this->controller->getattributeKey()->akName . ' -');
 foreach($options as $opt) { 
-	 
- 		$opts[$opt->getSelectAttributeOptionID()] = $opt->getSelectAttributeOptionDisplayValue() . ($opt->stockLevel <= 0 ? ' (' . t('Unavailable') .')': '');
+	  $opts[$opt->getSelectAttributeOptionID()] = $opt->getSelectAttributeOptionDisplayValue() . ($opt->stockLevel <= 0 ? ' (' . t('Unavailable') .')': '');
 }
 
 ?>
 <?php $selectoutput =  $form->select($this->field('atSelectOptionID') . '[]', $opts, $selectedOptions[0]); 
 
- 
+
  foreach($options as $opt) {
  	 if ($opt->stockLevel <= 0) {
  		$selectoutput = str_replace('option value="'.$opt->ID.'"', 'option value="'.$opt->ID.'" disabled="disabled"', $selectoutput);
- 		 
  		//$selectoutput = str_replace($opt->value . '</option>', $opt->value .' (Unavailable) </option>', $selectoutput);
  	}
+
+
  }
  
  echo $selectoutput;
-
 
 ?>
